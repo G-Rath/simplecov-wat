ce2c75b91cf581fcb7ef05df460486644a77411c
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-06-28 09:39:54 +1200

    feat: enable method coverage

--- Rakefile
+++ Rakefile
@@ -10,8 +10,9 @@ namespace :turbo_tests do
     # report coverage usage based on the results of all tests
     SimpleCov.collate Dir["coverage/turbo_tests/*/.resultset.json"] do
       enable_coverage :branch
+      enable_coverage :method
 
-      minimum_coverage line: 100, branch: 100
+      minimum_coverage line: 100, branch: 100, method: 100
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,14 @@ require "simplecov"
 
 SimpleCov.start do
   enable_coverage :branch
-  # enable_coverage :method
+  enable_coverage :method
   # enable_coverage :eval if Coverage.respond_to?(:supported?) && Coverage.supported?(:eval)
 
   use_merging false
   # minimum_coverage_by_file 80
   # maximum_coverage_drop 5
   # refuse_coverage_drop
-  minimum_coverage line: 100, branch: 100
+  minimum_coverage line: 100, branch: 100, method: 100
 
   # when running in parallel, write coverage to a directory-per-env which
   # we can then collate into a single report after all tests are done
@@ -24,4 +24,3 @@ SimpleCov.start do
     formatter SimpleCov::Formatter::SimpleFormatter
   end
 end
- 
